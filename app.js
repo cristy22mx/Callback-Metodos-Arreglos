c932fe24d4aa01fe2dc49323a5d81017d8aa51bd
cristy22mx <cristy22mx@gmail.com>
2018-02-20 08:16:57 -0600

    tarea

---
+++ app.js
@@ -0,0 +1,50 @@
+/*Función forEach*/
+
+
+
+
+
+
+/*Función map*/
+
+var numeros = [12,3,7,];
+
+var cuadrados = [];
+
+for (var i = numeros.length - 1; i>=0; i--){
+	var numero = numeros[i];
+	cuadrados.push(Math.pow(numero,2));
+}
+
+console.log(cuadrados);
+
+/*Función filter*/
+
+var numeros = [12,3,7,8,9,11];
+
+var numeros_pares = [];
+
+for (var i = numeros.length; i>=0; i--){
+	var numero = numeros[i];
+	if(numero % 2 == 0){
+		numeros_pares.push(numero);
+	}
+}
+
+console.log(numeros_pares);
+
+/*Función reduce*/
+
+var letras = ["H", "O", "L", "A"];
+
+console.log(letras.join(""));
+
+
+/*otra forma*/
+var numeros = [12,3,7,8,9,11];
+
+var suma = 0;
+for(var i = 0; i<numeros.length;i++){
+	suma +=numeros[i];
+}
+console.log(suma);
